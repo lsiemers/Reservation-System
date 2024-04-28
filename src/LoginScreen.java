119595168ed5d3887e0bac70b9676ab2f9860a15
Lukas Siemers <lsiemers@missouriwestern.edu>
2024-04-28 00:18:44 -0500

    Created Login Screen Controller

---
+++ src/LoginScreen.java
@@ -0,0 +1,30 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginScreen implements Initializable {
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private PasswordField passwordFieldtxt;
+
+    @FXML
+    private TextField usernameFieldtxt;
+
+
+@Override
+public void initialize(URL arg0, ResourceBundle arg1) {
+    System.out.println("TEST");
+
+}
+
+
+
+}
